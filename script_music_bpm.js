561b4f3f723a2269fd58e087bd51bb517fc5708a
Петрович <71115406+Alkor7@users.noreply.github.com>
2021-05-08 16:02:06 +0300

    Add files via upload

--- script_music_bpm.js
+++ script_music_bpm.js
@@ -1,4 +1,5 @@
-var timeStart, timeBeat, beatsCount=-1, beatsResult;
+'use strict'
+var timeStart, timeBeat, beatsCount=-1, beatsResult, oldBeat, progressCount = 0;
 
 window.addEventListener('keydown', function(keyb_control) { 
 	if (keyb_control.keyCode == 32) { RhythmCalc() } // Нажали пробел 
@@ -6,19 +7,35 @@ window.addEventListener('keydown', function(keyb_control) {
 } )
 
 function RhythmCalc() { 
-	beatsCount++;		// Считаем количество нажатий beatsCount
+	beatsCount++;		// Считаем количество нажатий
 	if (beatsCount == 0) { timeStart = Date.now(); }	// Фиксируем начало измерений
 	timeBeat = (Date.now() - timeStart);	// Считаем количество миллисекунд от старта
-	beatsResult = Math.floor(60/(timeBeat/1000/beatsCount)); // Целочисленно делим ВРЕМЯ на КОЛИЧЕСТВО 
-	if (beatsResult%2 == 1) { beatsResult++;}	// Округляем до четного числа 
+	
+	beatsResult = Math.floor(60/(timeBeat/1000/beatsCount)); // делим ВРЕМЯ на КОЛИЧЕСТВО 
+	if (beatsResult%2 == 1) { beatsResult++;}	// Округляем до целого четного числа 
 	console.log ("Нажатий= ", beatsCount, " Прошло ms=", timeBeat, " BPM=", beatsResult);
+
+	if (beatsResult == oldBeat) { 
+		progressCount+=10;
+		if (progressCount >= 99) { 
+			progressCount = 100;
+			document.getElementById('yourBPM').style.fontSize ='2em';
+			document.getElementById('yourBPM').classList.add("animate__flip");
+			document.getElementById('buttonTemp').onclick = "none";
+		}
+		document.getElementById('progressBar').value = progressCount;
+	}
+
 	if (beatsCount >=1) { 
 		document.querySelector('#yourBPM').innerText = beatsResult;
 	}
+	oldBeat = beatsResult;
 }
 
 function RhythmReset() { 
 	beatsCount = -1;
 	beatsResult = "";
+	progressCount = 0;
 	document.querySelector('#yourBPM').innerText = "_ _ _";
+	document.getElementById('progressBar').value = progressCount;
 }
