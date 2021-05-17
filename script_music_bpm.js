0526d2610c1b1a77f2ed60d063c8f6fed9671e60
Петрович <71115406+Alkor7@users.noreply.github.com>
2021-05-17 18:53:35 +0300

    Add files via upload

--- script_music_bpm.js
+++ script_music_bpm.js
@@ -1,41 +1,48 @@
 'use strict'
-var timeStart, timeBeat, beatsCount=-1, beatsResult, oldBeat, progressCount = 0;
+var timeStart, timeBeat, beatsCount=-1, beatsResult, oldBeat, progressCount = 0, isFound = false;
+// beatsCount=-1 чтобы корректно считать со 2 нажатия 
+var progressBarDOM = document.getElementById('progressBar'); // нашли на странице прогресс бар
+var yourBPM_DOM = document.getElementById('yourBPM'); // нашли блок для вывода результата 
+var calcBPM_DOM = document.getElementById('buttonTemp'); // нашли кнопку, на которую повешена функция
+var buttonReset_DOM = document.getElementById('buttonReset'); // нашли кнопку с функциями сброса
 
-window.addEventListener('keydown', function(keyb_control) { 
-	if (keyb_control.keyCode == 32) { RhythmCalc() } // Нажали пробел 
-	if (keyb_control.keyCode == 46) { RhythmReset() } // Нажали DEL
-} )
+calcBPM_DOM.addEventListener('click', RhythmCalc); // вызов RhythmCalc при нажатии buttonTemp
+buttonReset_DOM.addEventListener('click', RhythmReset); //вызов RhythmReset при нажатии buttonReset
 
-function RhythmCalc() { 
-	beatsCount++;		// Считаем количество нажатий
-	if (beatsCount == 0) { timeStart = Date.now(); }	// Фиксируем начало измерений
-	timeBeat = (Date.now() - timeStart);	// Считаем количество миллисекунд от старта
-	
-	beatsResult = Math.floor(60/(timeBeat/1000/beatsCount)); // делим ВРЕМЯ на КОЛИЧЕСТВО 
-	if (beatsResult%2 == 1) { beatsResult++;}	// Округляем до целого четного числа 
-	console.log ("Нажатий= ", beatsCount, " Прошло ms=", timeBeat, " BPM=", beatsResult);
-
-	if (beatsResult == oldBeat) { 
-		progressCount+=10;
-		if (progressCount >= 99) { 
-			progressCount = 100;
-			document.getElementById('yourBPM').style.fontSize ='2em';
-			document.getElementById('yourBPM').classList.add("animate__flip");
-			document.getElementById('buttonTemp').onclick = "none";
-		}
-		document.getElementById('progressBar').value = progressCount;
+function RhythmCalc() {
+	if (!isFound) { // Логика isFound нашли - true, еще ищем = false
+		yourBPM_DOM.classList.remove('animate__fadeOutUp'); 
+		beatsCount++;		// Считаем количество нажатий
+		if (beatsCount == 0) { timeStart = Date.now(); }	// Фиксируем начало измерений
+		timeBeat = (Date.now() - timeStart);	// Считаем количество миллисекунд от старта
+		beatsResult = Math.floor(60/(timeBeat/1000/beatsCount)); // делим ВРЕМЯ на КОЛИЧЕСТВО 
+		if (beatsResult%2 == 1) { beatsResult--;}	// Округляем до целого четного числа 
+		
+		if (beatsResult == oldBeat) { progressCount+=8; } // Индикация степени угадывания 
+		if (progressCount >= 100) { BPMisFound() } // Определили BPM, вывели и зафиксир результат 
+		progressBarDOM.value = progressCount; // Заполняем прогресс-бар
+		if (beatsCount >=1) { yourBPM_DOM.innerText = beatsResult;} // Вывод текущего BPM
+		oldBeat = beatsResult;
+		console.log ("Нажатий= ", beatsCount, " Прошло ms=", timeBeat, " BPM=", beatsResult);
 	}
 
-	if (beatsCount >=1) { 
-		document.querySelector('#yourBPM').innerText = beatsResult;
-	}
-	oldBeat = beatsResult;
 }
 
-function RhythmReset() { 
-	beatsCount = -1;
-	beatsResult = "";
-	progressCount = 0;
-	document.querySelector('#yourBPM').innerText = "_ _ _";
-	document.getElementById('progressBar').value = progressCount;
+function RhythmReset() { // Сброс всех параметров и счетчиков, Вкл кнопку
+	beatsCount = -1; // сбросили счетчик нажатий
+	beatsResult = ""; // обнулили результат
+	progressCount = 0; // прогресс-бар на ноль 
+	progressBarDOM.value = progressCount; // заполнили прогресс-бар на ноль
+	yourBPM_DOM.classList.remove('animate__flip'); // удалили анимацию 
+	yourBPM_DOM.classList.add('animate__fadeOutUp');
+	yourBPM_DOM.style.color = "#777";
+	isFound = false; // "Найдено?" = Ложь
+	calcBPM_DOM.disabled = false; // Сделали кнопку доступной 
+}
+
+function BPMisFound() { // Анимировали результат, Отключили кнопку
+	yourBPM_DOM.classList.add('animate__flip');
+	yourBPM_DOM.style.color = "#006400";
+	isFound = true; 
+	calcBPM_DOM.disabled = true;
 }
